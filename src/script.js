5689ce6c0c70444afbbb7bdadb85d530fb0a0781
MLDuke <123387486+MLDuke@users.noreply.github.com>
2023-05-21 13:38:19 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 $(".slider-gallery_component").each(function (index) {
-  const swiper = new Swiper($(this).find(".swiper")[0], {
-    //
+  const swiper = new Swiper($(this).find(".swiper.is-slider-bg")[0], {
+    slidesPerView: 1,
+    speed: 900,
+    keyboard: true
   });
 });
